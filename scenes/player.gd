34de94c7cca9cfc7845bcdb07d8dbc5794e7f4e7
paladin <krasnogorov.es@yahoo.com>
2024-12-15 18:31:29 +0300

    1.1 player animations

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,36 +1,41 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
-const JUMP_VELOCITY = -220.0
+const JUMP_VELOCITY = -280.0
+const RUN_MULTIPLIER = 1.33
 
 @onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		animated_sprite.play("jumping")
-		
-	# Handle jump.
+	var direction = Input.get_axis("move_left", "move_right")
+
+	# Handle Jump
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("move_left", "move_right")
+	if is_on_floor():  # Handle Horizontal Movement and Animation
+		if direction != 0:
+			velocity.x = direction * SPEED
+			animated_sprite.flip_h = direction < 0
+			if Input.is_action_pressed("run"):
+				velocity.x *= RUN_MULTIPLIER
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("walk")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			animated_sprite.play("idle")
+	else:
+		# While in the air, apply gravity and play the jumping animation
+		velocity.x = direction * SPEED # No sideways movement while jumping
+		animated_sprite.flip_h = direction < 0
+		animated_sprite.play("jumping")	
+		if Input.is_action_pressed("run"):
+				velocity.x *= RUN_MULTIPLIER
 	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+	
 
-	if direction==0:
-		animated_sprite.play("idle")
-	else:
-		animated_sprite.play("run")
-
-	if direction:
-		velocity.x = direction * SPEED
-		
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# Apply gravity regardless of state
+	velocity += get_gravity() * delta
+	
 	move_and_slide()
