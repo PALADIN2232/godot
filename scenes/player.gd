d36642dd3b55593a9d10de5affb498940d4c084e
paladin <krasnogorov.es@yahoo.com>
2024-12-16 21:42:21 +0300

    1.21 attack fix

--- scenes/player.gd
+++ scenes/player.gd
@@ -13,47 +13,47 @@ var is_attacking = false  # Флаг для отслеживания атаки
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
 
-	# Если идет атака, блокируем управление
+	# Handle Jump
+	if Input.is_action_pressed("jump") and is_on_floor() and not is_attacking:
+		velocity.y = JUMP_VELOCITY
+
+	# Если идет атака, не переключаем анимации, но движения продолжаются
 	if is_attacking:
-		# На земле полностью блокируем горизонтальное движение
+		# Позволяем гравитации работать во время атаки
+		velocity += get_gravity() * delta
 		if is_on_floor():
 			velocity.x = 0
-		else:
-			# В воздухе сохраняем горизонтальную скорость
-			velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-
-		# Гравитация продолжает действовать
-		velocity += get_gravity() * delta
+			if direction > 0:
+				velocity.x += 1
+			if direction < 0:
+				velocity.x += -1
 		move_and_slide()
 		return
-
-	# Handle Jump
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	if is_on_floor():  # Handle Horizontal Movement and Animation
 		if direction != 0:
 			velocity.x = direction * SPEED
-			animated_sprite.flip_h = direction < 0
+			animated_sprite.flip_h = direction < 0  # Поворачиваем персонажа только на земле
 			if Input.is_action_pressed("run"):
 				velocity.x *= RUN_MULTIPLIER
 				animated_sprite.play("run")
 			else:
 				animated_sprite.play("walk")
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED * delta)  # Плавное торможение, если игрок остановился
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 			animated_sprite.play("idle")
 	else:
 		# While in the air, apply gravity and play the jumping animation
 		velocity.x = direction * SPEED
-		animated_sprite.flip_h = direction < 0
+		# Не изменяем flip_h в воздухе, чтобы избежать нежелательных поворотов
 		animated_sprite.play("jumping")
+		animated_sprite.flip_h = direction < 0
 		
 		if Input.is_action_pressed("run"):
 			velocity.x *= RUN_MULTIPLIER
 
 	# Handle Attack
-	if Input.is_action_pressed("attack"):
+	if Input.is_action_just_pressed("attack"):
 		start_attack()
 
 	# Apply gravity regardless of state
@@ -64,9 +64,6 @@ func _physics_process(delta: float) -> void:
 func start_attack() -> void:
 	# Запускаем атаку
 	is_attacking = true
-	# Останавливаем движение только на земле
-	if is_on_floor():
-		velocity.x = 0
 	animated_sprite.play(attack_animations[current_attack])
 	current_attack = (current_attack + 1) % attack_animations.size()
 
